49cc3b8b5de52652842e67f5366663031a25bd44
Joan Catherina Zorovich Gutierrez <joan.zorovich@globant.com>
2022-12-29 21:21:56 -0500

    Homepage test working

--- cypress/support/pages/HomePage.js
+++ cypress/support/pages/HomePage.js
@@ -1,9 +1,28 @@
 var pageLocators = {
-    newTodoInput: '.new-todo',
+    carouselNextButton: '.carousel-control-next-icon',
+    carouselPreviousButton: '.carousel-control-prev-icon',
+    carouselItems: '.carousel-item',
    }
 
 class HomePage {
-    newTodoInput() { return cy.get(pageLocators.newTodoInput); }
+    carouselNextButton() { return cy.get(pageLocators.carouselNextButton); }
+    carouselPreviousButton() { return cy.get(pageLocators.carouselPreviousButton); }
+    carouselItems() { return cy.get(pageLocators.carouselItems, { timeout: 10000 }); }
+
+
+    
+    clickCarouselNextButton() {
+        return this.carouselNextButton().click();
+      }
+    
+    clickCarouselPrevButton() {
+        return this.carouselPreviousButton().click();
+      }
+    
+    prueba(){
+      console.log("aquiiiiiiii" + this.carouselItemsLength);
+    }
+
    } 
 export default HomePage;
    
